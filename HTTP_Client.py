07031b78e7e3c27b5ba19e20240afe8942a4c4a4
kaulmesanyam <kaulsanyam@gmail.com>
2023-10-25 21:28:14 +0530

    client and proxy updated

--- HTTP_Client.py
+++ HTTP_Client.py
@@ -1,12 +1,42 @@
-import urllib.request
+# import urllib.request
+#
+# localhost = 'http://localhost:8000/'
+# google = 'http://www.google.com/'
+# proxy = 'http://localhost:8888/'
+# def send_request():
+#     url = proxy
+#     response = urllib.request.urlopen(url)
+#     print(response.read().decode())
+#
+# if __name__ == '__main__':
+#     send_request()
 
-localhost = 'http://localhost:8000/'
-google = 'http://www.google.com/'
-proxy = 'http://localhost:8888/'
-def send_request():
-    url = proxy
-    response = urllib.request.urlopen(url)
-    print(response.read().decode())
+
+import socket
+import sys
+
+def send_request(proxy_ip, server_ip, port, path):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((proxy_ip, port))
+
+    request = f"GET {path} HTTP/1.1\r\nHost: {server_ip}\r\n\r\n"
+    client_socket.send(request.encode())
+
+    response = client_socket.recv(4096)
+    while response:
+        print(response.decode(), end='')
+        response = client_socket.recv(4096)
+
+    client_socket.close()
 
 if __name__ == '__main__':
-    send_request()
+    if len(sys.argv) < 5:
+        print("Usage: python web_client.py <proxy_ip> <server_ip> <port> <path>")
+        sys.exit(1)
+
+    proxy_ip = sys.argv[1]
+    server_ip = sys.argv[2]
+    port = int(sys.argv[3])
+    path = sys.argv[4]
+
+    send_request(proxy_ip, server_ip, port, path)
--- Proxy_Server.py
+++ Proxy_Server.py
@@ -3,29 +3,45 @@ import threading
 
 PORT = 8888
 def handle_client(client_socket):
-    request = client_socket.recv(4096)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect(('127.0.0.1', 8000))  # Change to the desired destination server
+    request = client_socket.recv(4096).decode()
+    print(request)
+    # Parse the request to extract the host and port
+    print('Parsing request...')
+    command = request.split('\r\n')[0].split(' ')[0]
+    print(f'command: {command}')
+    if command == 'GET':
+        host = request.split('\r\n')[1].split(' ')[1]
+        port = 80
 
-    server_socket.send(request)
-    print(f'Client request sent to original server')
-    response = server_socket.recv(4096)
-    client_socket.send(response)
-    print(f'response sent back to client')
-    client_socket.close()
-    server_socket.close()
+        print(f'host: {host}  | port: {port}')
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((host, port))
+
+        server_socket.sendall(request.encode())
+
+        response = server_socket.recv(4096)
+        while response:
+            client_socket.sendall(response)
+            response = server_socket.recv(4096)
+
+        client_socket.close()
+        server_socket.close()
+    else:
+        client_socket.close()
+
+    print("socket closed")
 
 
 def run_proxy():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', PORT))  # Proxy server listens on localhost, port 8888
-    server.listen(5)
+    proxy_address = ('127.0.0.1', PORT)
+    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxy_socket.bind(proxy_address)
+    proxy_socket.listen(5)
 
     print(f'Proxy server is running on port {PORT}...')
 
     while True:
-        client_socket, addr = server.accept()
-        print(f'Request from {addr} accepted')
+        client_socket, addr = proxy_socket.accept()
         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
 
